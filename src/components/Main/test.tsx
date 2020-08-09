0e18d9f9da19537391966ce8a36fc305da1fa4e7
Lucas C Correa <lucasccorrea@gmail.com>
2020-08-09 01:00:05 -0300

    added global styles and Main component styled

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,9 +7,16 @@ describe('<Main />', () => {
     const { container } = render(<Main />);
 
     expect(
-      screen.getByRole('heading', { name: /react test/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b',
+    });
   });
 });
